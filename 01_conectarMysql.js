bd804cbff0229cba94f0f239cd80e8c248cffeb0
juanmanuel <juanmanuel.znn@gmail.com>
2026-04-21 13:58:59 -0300

    conectar mysql

---
+++ 01_conectarMysql.js
@@ -0,0 +1,20 @@
+import mysql from 'mysql2';
+
+const conexion = mysql.createConnection(
+    // host: process.env
+    // user:
+    // password:
+    // database:
+)
+
+const sqlQuery = 'SELECT * FROM actor';
+
+conexion.query(sqlQuery, (queryErr, results) => {
+    if (queryErr) {
+        console.error("Error ejecutando la consulta:", queryErr)
+        return;
+    }
+
+    console.log("Resultados:", results);
+    
+});
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # programacion-3-cursada
+
+## Semana 3 - APIs Rest
+### Comandos
+- npm install
+
